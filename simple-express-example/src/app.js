7cd149c23df7dbd0d9eb965905e12b7235b79492
Pablo Abratte <pabratte@gmail.com>
2025-05-08 18:38:22 -0300

    add simple-express-example

--- simple-express-example/src/app.js
+++ simple-express-example/src/app.js
@@ -3,6 +3,18 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+function hello(req, res){
+    res.set('Content-Type', 'text/html');
+    res.status(200).send("<h1>Hello From Express!</h1>");
+}
+
+app.get('/', (req, res)=>{
+    res.set('Content-Type', 'text/html');
+    res.status(200).send("<p>This is the main page.</p>");
+});
+
+app.get('/hello', hello);
+
 app.listen(PORT, (error) =>{
     if(!error)
         console.log("Server is Successfully Running, and App is listening on port "+ PORT)
@@ -10,13 +22,3 @@ app.listen(PORT, (error) =>{
         console.log("Error occurred, server can't start", error);
     }
 );
-
-app.get('/', (req, res)=>{
-    res.set('Content-Type', 'text/html');
-    res.status(200).send("<p>This is the main page.</p>");
-});
-
-app.get('/hello', (req, res)=>{
-    res.set('Content-Type', 'text/html');
-    res.status(200).send("<h1>Hello From Express!</h1>");
-});
